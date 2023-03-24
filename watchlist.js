dcf9155ed6b9d0d01b0e69c0cae44892050ba067
bsmuse <bsmuse@tutamail.com>
2023-03-24 08:13:10 -0600

    commit

--- watchlist.js
+++ watchlist.js
@@ -1,23 +1,16 @@
 let watchList = JSON.parse(localStorage.getItem('watchList'))
 console.log(watchList)
-
 const render = () => {
-    const moviesContainer = document.getElementById('movies')
-    moviesContainer.innerHTML = ''
-    
     if (watchList == null) {
-        document.querySelector('.search-container').style.display = 'block'
-    } else {
-        document.querySelector('.search-container').style.display = 'none'
-        watchList.forEach((movie) => {
-            const movieDiv = document.createElement('div')
-            movieDiv.classList.add('movie')
-            movieDiv.textContent = movie
-            movieDiv.addEventListener('click', click)
-            moviesContainer.appendChild(movieDiv)
+        document.querySelector('.search-container').style.display='block'
+    } else{
+        document.querySelector('.search-container').style.display='none'
+        watchList.forEach( (movie) => {
+        document.getElementById('movies').innerHTML += `<div class='movie'>${movie}</div>`
+        click()
         })
     }
-}
+} 
 
 const click = () => {
     document.querySelectorAll(".list-btn").forEach(function(button){
